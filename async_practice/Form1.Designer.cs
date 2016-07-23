b380c0953ea64c768d8ce71108d778dcb4a67803
gregSchulte <rikkitikkitumbo@gmail.com>
2016-07-22 17:09:01 -0700

    initial commit

---
+++ async_practice/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace async_practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnTikTok = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(301, 376);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 91);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(585, 354);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 71);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(829, 376);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 97);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "0";
+            // 
+            // btnTikTok
+            // 
+            this.btnTikTok.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTikTok.Location = new System.Drawing.Point(557, 635);
+            this.btnTikTok.Name = "btnTikTok";
+            this.btnTikTok.Size = new System.Drawing.Size(130, 61);
+            this.btnTikTok.TabIndex = 1;
+            this.btnTikTok.Text = "Tik Tok";
+            this.btnTikTok.UseVisualStyleBackColor = true;
+            this.btnTikTok.Click += new System.EventHandler(this.btnTikTok_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(557, 843);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(175, 41);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop! Stop! Stop!";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1340, 1032);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnTikTok);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnTikTok;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ async_practice/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace async_practice
+{
+    public partial class Form1 : Form
+    {
+        static List<Label> labelList = new List<Label>();
+        List<Thread> threadsList = new List<Thread>();
+
+        static readonly object _locker = new object();
+        static bool keepGoing = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "0";
+            label2.Text = "0";
+            label3.Text = "0";
+            labelList.Add(label1);
+            labelList.Add(label2);
+            labelList.Add(label3);
+        }
+
+       
+
+        private void btnTikTok_Click(object sender, EventArgs e)
+        {
+            keepGoing = true;
+            Thread t = new Thread( ()=> tik(0));
+            Thread t2 = new Thread(() => tik(1));
+            Thread t3 = new Thread(() => tik(2));
+            threadsList.Add(t);
+            threadsList.Add(t2);
+            threadsList.Add(t3);
+            foreach (Thread th in threadsList)
+            {
+                if (th.ThreadState == ThreadState.Unstarted) { th.Start(); }
+            }
+            
+        }
+
+        void tik(int labelIndex)
+        {
+            while (keepGoing)
+            {
+                int j = int.Parse(labelList[labelIndex].Text);
+                j += 1;
+                Thread.Sleep((1000*(labelIndex + 1)/3));
+                update(j, labelIndex);
+            }
+        }
+
+        void update(int Label, int labelIndex)
+        {
+            if (keepGoing)
+            {
+                Action action = () =>
+                {
+                    labelList[labelIndex].Text = Label.ToString();
+                    Console.WriteLine(Label.ToString());
+                };
+                this.Invoke(action);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            keepGoing = false;
+
+        }
+
+
+
+    }
+}
